2c779d888e4a24b359cfe7874460a4a1a71df1c7
Raghav Ramesh <raghavrnus@gmail.com>
2020-07-28 23:33:00 +0800

    testing built project

---
+++ precache-manifest.f218192a22f46af35966f3cc62d82e60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1411aab20126e12df331ea3854ecd85d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73f17001bdf613a6f288",
+    "url": "/static/js/2.8294dc56.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8294dc56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "632d79028ceba2cf90d3",
+    "url": "/static/js/main.7a94301b.chunk.js"
+  },
+  {
+    "revision": "d67ece13b7ed09b31ac9",
+    "url": "/static/js/runtime-main.5db25d3d.js"
+  }
+]);
