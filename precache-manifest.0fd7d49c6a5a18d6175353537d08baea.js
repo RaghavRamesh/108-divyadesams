d61572af3c4afd284cb93adb79da8870f7d47b39
Raghav Ramesh <raghavrnus@gmail.com>
2020-07-29 09:19:27 +0800

    update build

---
+++ precache-manifest.0fd7d49c6a5a18d6175353537d08baea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa853cc1db5f2161c30463e5001f534",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73f17001bdf613a6f288",
+    "url": "/static/js/2.8294dc56.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8294dc56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f86d93667a36b75acd13",
+    "url": "/static/js/main.3cf2c417.chunk.js"
+  },
+  {
+    "revision": "d67ece13b7ed09b31ac9",
+    "url": "/static/js/runtime-main.5db25d3d.js"
+  }
+]);
